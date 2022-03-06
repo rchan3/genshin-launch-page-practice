33eeda133d7c8c623278a4c2d84661298f0ba8d6
rchan3 <r.achiechan@gmail.com>
2022-03-05 20:58:50 -0500

    added left+right carousel arrows with hover css

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
 $(document).ready(function () {
   $(".carousel").slick({
     autoplay: true,
+    prevArrow: $(".prev"),
+    nextArrow: $(".next"),
+  });
+  $(".shenhe-portrait-click").click(function () {
+    $(".carousel").slick("slickGoTo", 0);
+  });
+  $(".ganyu-portrait-click").click(function () {
+    $(".carousel").slick("slickGoTo", 1);
+  });
+  $(".zhongli-portrait-click").click(function () {
+    $(".carousel").slick("slickGoTo", 2);
+  });
+  $(".xiao-portrait-click").click(function () {
+    $(".carousel").slick("slickGoTo", 3);
+  });
+  $(".yunjin-portrait-click").click(function () {
+    $(".carousel").slick("slickGoTo", 4);
   });
 });
